3a62f39f6c47ea15f27ca9f9c074f44390e36ba2
Ulrich LELE <lele.ulrich2@gmail.com>
2023-04-18 19:01:31 +0200

    Create protocol maker class

---
+++ src/main/java/org/lacombe/coffemaker/ProtocolMaker.java
@@ -0,0 +1,10 @@
+package org.lacombe.coffemaker;
+
+public class ProtocolMaker {
+
+    public String generateOrder(String drinkType, int numberOfSugar){
+        String order = "";
+        
+        return order;
+    }
+}
